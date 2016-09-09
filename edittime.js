8114c220bb1533db70963dda696ad6217c4bf630
Mimiste <mimiste@me.com>
2016-09-09 17:54:30 +0200

    First commit of the C2 plugin architecture (not functionnal)

---
+++ edittime.js
@@ -0,0 +1,127 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Scorm C2",				// as appears in 'insert object' dialog, can be changed as long as "id" stays the same
+		"id":			"scormc2",				// this is used to identify this plugin and is saved to the project; never change it
+		"version":		"0.1",					// (float in x.y format) Plugin version - C2 shows compatibility warnings based on this
+		"description":	"A Scorm 1.2 and 2004 Plugin for Construct 2",
+		"author":		"Loïc Martinez",
+		"help url":		"",
+		"category":		"General",				// Prefer to re-use existing categories, but you can set anything here
+		"type":			"object",				// either "world" (appears in layout and is drawn), else "object"
+		"rotatable":	false,					// only used when "type" is "world".  Enables an angle property on the object.
+		"flags":		0						// uncomment lines to enable flags...
+						| pf_singleglobal		// exists project-wide, e.g. mouse, keyboard.  "type" must be "object".
+					//	| pf_texture			// object has a single texture (e.g. tiled background)
+					//	| pf_position_aces		// compare/set/get x, y...
+					//	| pf_size_aces			// compare/set/get width, height...
+					//	| pf_angle_aces			// compare/set/get angle (recommended that "rotatable" be set to true)
+					//	| pf_appearance_aces	// compare/set/get visible, opacity...
+					//	| pf_tiling				// adjusts image editor features to better suit tiled images (e.g. tiled background)
+					//	| pf_animations			// enables the animations system.  See 'Sprite' for usage
+					//	| pf_zorder_aces		// move to top, bottom, layer...
+					//  | pf_nosize				// prevent resizing in the editor
+					//	| pf_effects			// allow WebGL shader effects to be added
+					//  | pf_predraw			// set for any plugin which draws and is not a sprite (i.e. does not simply draw
+												// a single non-tiling image the size of the object) - required for effects to work properly
+	};
+};
+
+////////////////////////////////////////
+// Conditions
+
+AddCondition(0, cf_none, "Is scorm initialised", "Initialisation", "Is scorm initialised", "Verifie si la connection au tracker est établie", "isScormInitialised");
+AddCondition(1, cf_none, "Is scorm initialising", "Initialisation", "Is scorm initialising", "Verifie si la connection au tracker est en cours...", "isScormInitialising");
+AddCondition(2, cf_none, "Is scorm on error", "Error handling", "Is scorm on error", "Verifie si une erreur a eu lieu", "isScormOnError");
+
+////////////////////////////////////////
+// Actions
+
+AddAction(0, af_none, "Initialise LMS", "Initialisation", "Initialise LMS", "Initialise la connection au tracker", "initialiseLMS");
+AddStringParam("Name", "Name of the scorm value to set.");
+AddStringParam("Value", "The value.");
+AddAction(1, af_none, "Set LMS Value", "Usage", "Set LMS Value {0} - {1}", "Met à jour une valeur sur le LMS (voir les identifiants possible dans la doc de SCORM 1.2)", "setLMSValue");
+AddAction(2, af_none, "Do LMS Commit", "Usage", "Do LMS Commit", "Commit les valeurs mises à jour sur le LMS", "doLMSCommit");
+
+////////////////////////////////////////
+// Expressions
+
+AddExpression(0, ef_return_string, "Initialisation", "Error handling", "getLastError", "Retourne la dernière erreur de SCORM");
+AddExpression(1, ef_return_number, "Initialisation", "Error handling", "getLastErrorID", "Retourne l'ID de la dernière erreur de SCORM");
+AddNumberParam("Temps", "Le temps à convertir (en millisecondes)");
+AddExpression(2, ef_return_string, "Initialisation", "Usage", "formatTimeForScorm", "Retourne le temps passé en parametre (en millisecondes), au format SCORM 1.2");
+AddStringParam("Name", "Name of the scorm value to get.");
+AddExpression(3, ef_return_string, "Initialisation", "Usage", "getLMSValue", "Retourne la valeur dont l'identifiant est passé en parametre");
+
+////////////////////////////////////////
+ACESDone();
+
+var property_list = [
+	];
+
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+
+	// Set the default property values from the property table
+	this.properties = {};
+
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+
+	// Plugin-specific variables
+	// this.myValue = 0...
+}
+
+// Called when inserted via Insert Object Dialog for the first time
+IDEInstance.prototype.OnInserted = function()
+{
+}
+
+// Called when double clicked in layout
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called after a property has been changed in the properties bar
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+
+// For rendered objects to load fonts or textures
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+
+// Called to draw self in the editor if a layout object
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// For rendered objects to release fonts or textures
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
---
+++ runtime.js
@@ -0,0 +1,230 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+//          vvvvvvvv
+cr.plugins_.scormc2 = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+	//                            vvvvvvvv
+	var pluginProto = cr.plugins_.scormc2.prototype;
+
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		this.isInitialising = false;
+		this.isScormInitialised = false;
+		this.isOnError = false;
+		this.tracker = null;
+		this.lastError = "";
+		this.lastErrorID = 0;
+
+		// any other properties you need, e.g...
+		// this.myValue = 0;
+	};
+
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+		// note the object is sealed after this call; ensure any properties you'll ever need are set on the object
+		// e.g...
+		// this.myValue = 0;
+	};
+
+	// called whenever an instance is destroyed
+	// note the runtime may keep the object after this call for recycling; be sure
+	// to release/recycle/reset any references to other objects in this function.
+	instanceProto.onDestroy = function ()
+	{
+	};
+
+	// called when saving the full state of the game
+	instanceProto.saveToJSON = function ()
+	{
+		// return a Javascript object containing information about your object's state
+		// note you MUST use double-quote syntax (e.g. "property": value) to prevent
+		// Closure Compiler renaming and breaking the save format
+		return {
+			// e.g.
+			//"myValue": this.myValue
+		};
+	};
+
+	// called when loading the full state of the game
+	instanceProto.loadFromJSON = function (o)
+	{
+		// load from the state previously saved by saveToJSON
+		// 'o' provides the same object that you saved, e.g.
+		// this.myValue = o["myValue"];
+		// note you MUST use double-quote syntax (e.g. o["property"]) to prevent
+		// Closure Compiler renaming and breaking the save format
+	};
+
+	// only called if a layout object - draw to a canvas 2D context
+	instanceProto.draw = function(ctx)
+	{
+	};
+
+	// only called if a layout object in WebGL mode - draw to the WebGL context
+	// 'glw' is not a WebGL context, it's a wrapper - you can find its methods in GLWrap.js in the install
+	// directory or just copy what other plugins do.
+	instanceProto.drawGL = function (glw)
+	{
+	};
+
+	// The comments around these functions ensure they are removed when exporting, since the
+	// debugger code is no longer relevant after publishing.
+	/**BEGIN-PREVIEWONLY**/
+	instanceProto.getDebuggerValues = function (propsections)
+	{
+		// Append to propsections any debugger sections you want to appear.
+		// Each section is an object with two members: "title" and "properties".
+		// "properties" is an array of individual debugger properties to display
+		// with their name and value, and some other optional settings.
+		propsections.push({
+			"title": "My debugger section",
+			"properties": [
+				// Each property entry can use the following values:
+				// "name" (required): name of the property (must be unique within this section)
+				// "value" (required): a boolean, number or string for the value
+				// "html" (optional, default false): set to true to interpret the name and value
+				//									 as HTML strings rather than simple plain text
+				// "readonly" (optional, default false): set to true to disable editing the property
+
+				// Example:
+				// {"name": "My property", "value": this.myValue}
+			]
+		});
+	};
+
+	instanceProto.onDebugValueEdited = function (header, name, value)
+	{
+		// Called when a non-readonly property has been edited in the debugger. Usually you only
+		// will need 'name' (the property name) and 'value', but you can also use 'header' (the
+		// header title for the section) to distinguish properties with the same name.
+		if (name === "My property")
+			this.myProperty = value;
+	};
+	/**END-PREVIEWONLY**/
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+
+	//Return true if scorm is initialised
+	Cnds.prototype.isScormInitialised = function (myparam)
+	{
+		if (pluginProto.isScormInitialised){
+			return pluginProto.isScormInitialised;
+		} else {
+			if(pluginProto.tracker.LMSIsInitialized()){
+				pluginProto.isScormInitialised = true;
+			}
+			return pluginProto.isScormInitialised;
+		}
+	};
+
+	//Return true if scorm is isInitialising
+	Cnds.prototype.isScormInitialising = function (myparam)
+	{
+		return pluginProto.isScormInitialising;
+	};
+
+	//Return true if scorm is on error state
+	Cnds.prototype.isScormOnError = function (myparam)
+	{
+		return pluginProto.isOnError;
+	};
+
+	pluginProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	//Launch scorm init
+	Acts.prototype.initialiseLMS = function ()
+	{
+		var result = pluginProto.tracker.doLMSInitialize();
+		if (result == "true"){
+			pluginProto.isInitialising = true;
+		} else {
+			pluginProto.isOnError = true;
+			pluginProto.isInitialising = false;
+		}
+	};
+
+	//Send a value to scorm
+	//For the possible ID, refer to scorm documentation
+	Acts.prototype.setLMSValue = function(pID, pValue){
+		if (pluginProto.isScormInitialised){
+			pluginProto.tracker.doLMSSetValue(pID, pValue);
+		}
+	}
+
+	//Commit all the values sent since the last commit
+	Acts.prototype.doLMSCommit = function(){
+		if (pluginProto.isScormInitialised){
+			pluginProto.tracker.doLMSCommit();
+		}
+	}
+
+	pluginProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+
+	//Get the last scorm error message as string
+	Exps.prototype.getLastError = function (ret)
+	{
+		ret.set_string(pluginProto.lastError);
+	};
+
+	//Get the last scorm error ID as an integer
+	Exps.prototype.getLastErrorID = function (ret)
+	{
+		ret.set_int(pluginProto.lastErrorID);
+	};
+
+	//Get a value from scorm
+	//Refer to the scorm documentation for the possible values for ID
+	Exps.prototype.getLMSValue = function (ret, pID)
+	{
+		ret.set_int(pluginProto.tracker.doLMSGetValue(pID));
+	};
+
+	pluginProto.exps = new Exps();
+
+}());
